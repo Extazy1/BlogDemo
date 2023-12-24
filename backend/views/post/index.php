464721496bf0ee3d8c7e0b47843b0874117e096f
unknown <2056345020@qq.com>
2023-12-24 20:22:23 +0800

    v1.3

--- backend/views/post/index.php
+++ backend/views/post/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use common\models\Post;
+use common\models\Poststatus;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
@@ -10,7 +11,7 @@ use yii\grid\GridView;
 /** @var common\models\PostSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = 'Posts';
+$this->title = '文章管理';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="post-index">
@@ -18,31 +19,46 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Post', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('创建文章', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?= GridView::widget([
+     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+           // ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
-            'title',
-            'content:ntext',
-            'tags:ntext',
-            'status',
-            //'create_time:datetime',
-            //'update_time:datetime',
-            //'author_id',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, Post $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
+            //'id',
+            ['attribute'=>'id',
+            'contentOptions'=>['width'=>'30px'],
             ],
+            'title',
+        	//'author_id',
+        	['attribute'=>'authorName',
+        	'label'=>'作者',
+        	'value'=>'author.nickname',
+    		],
+           // 'content:ntext',
+            'tags:ntext',
+            //'status',
+            ['attribute'=>'status',
+            'value'=>'status0.name',
+            'filter'=>Poststatus::find()
+            		->select(['name','id'])
+            		->orderBy('position')
+            		->indexBy('id')
+            		->column(),
+   			 ],
+            // 'create_time:datetime',
+             //'update_time:datetime',
+             ['attribute'=>'update_time',
+             'format'=>['date','php:Y-m-d H:i:s'],
+        	],
+             
+
+            ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
 
--- common/models/PostSearch.php
+++ common/models/PostSearch.php
@@ -11,14 +11,19 @@ use common\models\Post;
  */
 class PostSearch extends Post
 {
+    public function attributes()
+	{
+		return array_merge(parent::attributes(),['authorName']);
+	}
+
     /**
      * {@inheritdoc}
      */
     public function rules()
     {
         return [
-            [['id', 'status', 'create_time', 'update_time', 'author_id'], 'integer'],
-            [['title', 'content', 'tags'], 'safe'],
+            [['id', 'status', 'create_time', 'update_time', 'author_id'], 'integer'],          
+            [['title', 'content', 'tags','authorName'], 'safe'],
         ];
     }
 
@@ -46,8 +51,16 @@ class PostSearch extends Post
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+        	'pagination' => ['pageSize'=>10],
+        	'sort'=>[
+        			'defaultOrder'=>[
+        					'id'=>SORT_DESC,        			
+        			],
+        			//'attributes'=>['id','title'],
+        	],
         ]);
 
+        
         $this->load($params);
 
         if (!$this->validate()) {
@@ -57,8 +70,9 @@ class PostSearch extends Post
         }
 
         // grid filtering conditions
+        // 需指明为post.id
         $query->andFilterWhere([
-            'id' => $this->id,
+            'post.id' => $this->id,
             'status' => $this->status,
             'create_time' => $this->create_time,
             'update_time' => $this->update_time,
@@ -68,6 +82,16 @@ class PostSearch extends Post
         $query->andFilterWhere(['like', 'title', $this->title])
             ->andFilterWhere(['like', 'content', $this->content])
             ->andFilterWhere(['like', 'tags', $this->tags]);
+        
+        $query->join('INNER JOIN', 'Adminuser', 'post.author_id = Adminuser.id');
+        $query->andFilterWhere(['like', 'Adminuser.nickname', $this->authorName]);
+        //$query->andFilterWhere(['post.id' => $this->id]);
+
+        $dataProvider->sort->attributes['authorName'] = 
+        [
+        	'asc'=>['Adminuser.nickname'=>SORT_ASC],
+        	'desc'=>['Adminuser.nickname'=>SORT_DESC],
+        ];
 
         return $dataProvider;
     }
